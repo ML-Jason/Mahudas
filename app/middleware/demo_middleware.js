6a7e89e0f4f6c5a74dea79a5cc0fd2728d138e68
Jason Tseng <jason@medialand.tw>
2022-08-30 12:59:20 +0800

    + 調整 router 的啟動順序，避免部分app.use的middleware失效

--- app/middleware/demo_middleware.js
+++ app/middleware/demo_middleware.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
   const demo = async (ctx, next) => {
     ctx.locals = 'passing value';
-    next();
+    await next();
   };
   return demo;
 };
--- index.js
+++ index.js
@@ -13,23 +13,27 @@ process.env.APP_ENV = process.env.APP_ENV || 'dev';
 
 const app = require('./lib/app');
 
-// 載入config
-app.config = configLoader(app.appInfo);
-app.emit('configDidLoad');
+const init = async () => {
+  // 載入config
+  app.config = configLoader(app.appInfo);
+  app.emit('configDidLoad');
 
-// 設定public目錄
-app.use(koaStatic(path.join(app.appInfo.root, (app.config.static || {}).dir || 'app/public')));
+  // 載入application
+  await applicationLoader(app);
+  app.emit('didLoad');
 
-// 載入application
-applicationLoader(app);
-app.emit('didLoad');
+  // 讓router生效，這個步驟需要在所有app.use都掛載後才觸發，否則可能會造成middleware失效
+  app
+    .use(app.router.routes())
+    .use(app.router.allowedMethods());
+  // 設定public目錄
+  app.use(koaStatic(path.join(app.appInfo.root, (app.config.static || {}).dir || 'app/public')));
 
-// 啟動Server
-const port = app.config.port || 8080;
-app.listen(port);
-console.log(`serverDidReady, Listening ${port}`);
-app.emit('serverDidReady');
+  // 啟動Server
+  const port = app.config.port || 8080;
+  app.listen(port);
+  console.log(`serverDidReady, Listening ${port}`);
+  app.emit('serverDidReady');
+};
 
-module.exports = app;
-
-console.log(app);
+init();
