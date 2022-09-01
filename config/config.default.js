3be1aa9ed0b1f0c8f2f69f33cd0c39eb4e0816ff
DiroKao <dirokao@gmail.com>
2022-09-01 16:35:37 +0800

    調整 config 合併使用 array 會有覆蓋的問題，導致資料遺失

--- config/config.default.js
+++ config/config.default.js
@@ -3,13 +3,13 @@ const config = {
   static: {
     dir: 'app/public',
   },
-  coreMiddleware: [
+  coreMiddleware: {
     /**
      * koa-bodyParser
      * https://www.npmjs.com/package/koa-bodyparser
      * 將接收的檔案大小拉高到100mb
      */
-    {
+    bodyParser: {
       enable: true,
       package: 'koa-bodyparser',
       config: {
@@ -24,7 +24,7 @@ const config = {
      * 設定referrerPolicy為'no-referrer-when-downgrade'，否則有時會有些script或圖片出現錯誤。
      * 關閉crossOriginEmbedderPolicy，否則html會無法引用第三方js。
      */
-    {
+    helmet: {
       enable: true,
       package: 'koa-helmet',
       config: {
@@ -33,7 +33,7 @@ const config = {
         crossOriginEmbedderPolicy: false,
       },
     },
-  ],
+  },
 };
 
 module.exports = config;
