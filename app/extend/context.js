ef4530ea334e6675bc34f651a10561f12c9743b0
Jason Tseng <jason@medialand.tw>
2023-03-15 13:25:14 +0800

    feat: 擴充runInBackground的容錯，可以接受不同的參數與用法

--- app/extend/context.js
+++ app/extend/context.js
@@ -1,13 +1,63 @@
 module.exports = {
-  runInBackground(scope) {
+  /**
+   * usage:
+   *
+   * 1. 傳入Promise:
+   * const someFunction = async () => { do somthing here };
+   * ctx.runInBackgroun(someFunction());
+   *
+   * 2. 傳入function與參數
+   * ctx.runInBackgroun(async (p1, p2)=> { do somthing here }, [param1, param2]);
+   */
+  runInBackground(scope, params = []) {
     const ctx = this;
-    // eslint-disable-next-line no-promise-executor-return
-    return new Promise((resolve) => setImmediate(resolve))
-      .then(() => scope(ctx))
-      .catch((err) => {
-        // eslint-disable-next-line no-param-reassign
+
+    const dealScope = () => {
+      if (!scope) return;
+
+      const onError = (err) => {
+      // eslint-disable-next-line no-param-reassign
         err.runInBackground = true;
         ctx.app.emit('onBackgroundError', err, ctx);
+      };
+
+      // 如果傳入的是Promise，等待catch就好
+      if (scope.then && typeof scope.then === 'function') {
+        scope.catch(onError);
+        return;
+      }
+
+      // 處理傳入的參數，準備pass給function使用
+      let passParams = params;
+      if (params !== undefined && !Array.isArray(params)) passParams = [params];
+
+      // 如果傳入的是一個Async function
+      // 將params傳入給scope執行，並等待catch
+      if (scope.constructor
+      && scope.constructor.name === 'AsyncFunction') {
+        scope(...passParams).catch(onError);
+        return;
+      }
+
+      // 如果是一般function，直接用try...catch包起來執行
+      if (typeof scope === 'function') {
+        try {
+          const rs = scope(...passParams);
+          // 如果function執行後是回傳一個promise，也要把錯誤接起來
+          if (rs && rs.then && typeof rs.then === 'function') {
+            rs.catch(onError);
+          }
+        } catch (err) {
+          onError(err);
+        }
+      }
+    };
+
+    return new Promise((resolve) => {
+      setImmediate(resolve);
+    })
+      .then(() => {
+        dealScope();
       });
   },
 };
