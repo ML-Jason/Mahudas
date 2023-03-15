b42a8128379a0b19f250af298b8319ac728532de
DiroKao <dirokao@gmail.com>
2023-03-15 17:39:11 +0800

    feat: 如果參數使用 promise，出現console 警語 調整部分判斷使用 is

--- app/extend/context.js
+++ app/extend/context.js
@@ -3,14 +3,18 @@ module.exports = {
    * usage:
    *
    * 1. 傳入Promise:
-   * const someFunction = async () => { do somthing here };
-   * ctx.runInBackgroun(someFunction());
+   * const someFunction = async () => { do something here };
+   * ctx.runInBackground(someFunction());
    *
    * 2. 傳入function與參數
-   * ctx.runInBackgroun(async (p1, p2)=> { do somthing here }, [param1, param2]);
+   * ctx.runInBackground(async (p1, p2)=> { do something here }, [param1, param2]);
    */
   runInBackground(scope, params = []) {
     const ctx = this;
+    const { coreLogger, utils } = ctx.app;
+    const { is } = utils;
+
+    if (is.promise(scope)) coreLogger.warn('\x1B[33m▉ RunInBackground not recommended use Promise, which may cause the execution order to be different from what is expected!\x1B[0m');
 
     const onError = (err) => {
       // eslint-disable-next-line no-param-reassign
@@ -21,22 +25,21 @@ module.exports = {
     const dealScope = () => {
       // 處理傳入的參數，準備pass給function使用
       let passParams = params;
-      if (params !== undefined && !Array.isArray(params)) passParams = [params];
+      if (!is.nullOrUndefined(params) && !is.array(params)) passParams = [params];
 
       // 如果傳入的是一個Async function
       // 將params傳入給scope執行，並等待catch
-      if (scope.constructor
-      && scope.constructor.name === 'AsyncFunction') {
+      if (is.asyncFunction(scope)) {
         scope(...passParams).catch(onError);
         return;
       }
 
       // 如果是一般function，直接用try...catch包起來執行
-      if (typeof scope === 'function') {
+      if (is.function(scope)) {
         try {
           const rs = scope(...passParams);
           // 如果function執行後是回傳一個promise，也要把錯誤接起來
-          if (rs && rs.then && typeof rs.then === 'function') {
+          if (is.promise(rs)) {
             rs.catch(onError);
           }
         } catch (err) {
@@ -48,7 +51,7 @@ module.exports = {
     if (!scope) return null;
 
     // 如果傳入的是Promise，等待catch就好
-    if (scope.then && typeof scope.then === 'function') {
+    if (is.promise(scope)) {
       scope.catch(onError);
       return null;
     }
