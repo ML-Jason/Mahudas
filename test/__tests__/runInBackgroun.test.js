705929f927fbb0edb55a388cedb512d0ec98f773
ML-diro <50189493+ML-diro@users.noreply.github.com>
2023-03-15 17:54:16 +0800

    在 ctx 加入 runInBackground 功能 (#49)
    * feat:
    在 ctx 加入 runInBackground 功能
    
    * feat: 擴充runInBackground的容錯，可以接受不同的參數與用法
    
    * fix: 修正Promise如果馬上拋出Error時，runInBackgroun來不及接的問題
    
    在一開始就先試著捕捉錯誤，避免Error沒人接住。
    
    * feat: 重新搬移了一下code，並補上測試
    
    * feat:
    如果參數使用 promise，出現console 警語
    調整部分判斷使用 is
    
    ---------
    
    Co-authored-by: DiroKao <dirokao@gmail.com>
    Co-authored-by: Jason Tseng <jason@medialand.tw>

---
+++ test/__tests__/runInBackgroun.test.js
@@ -0,0 +1,61 @@
+const getApp = require('../index');
+
+let app;
+beforeAll(async () => {
+  app = await getApp();
+});
+afterAll(() => {
+  app.close();
+});
+
+describe('runInBackground測試', () => {
+  test('傳入async function', (done) => {
+    const ctx = app.newContext();
+
+    const fn = async (p1, p2) => {
+      const output = `${p1},${p2}`;
+      try {
+        expect(output).toBe('a,b');
+        done();
+      } catch (err) {
+        done(err);
+      }
+      return output;
+    };
+
+    ctx.runInBackground(fn, ['a', 'b']);
+  });
+
+  test('傳入Promise', (done) => {
+    const ctx = app.newContext();
+
+    const fn = async (p1, p2) => {
+      const output = `${p1},${p2}`;
+      try {
+        expect(output).toBe('c,d');
+        done();
+      } catch (err) {
+        done(err);
+      }
+      return output;
+    };
+
+    ctx.runInBackground(fn('c', 'd'));
+  });
+
+  test('捕捉錯誤', (done) => {
+    const ctx = app.newContext();
+    app.on('onBackgroundError', (e) => {
+      try {
+        expect(e.message).toBe('some error');
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+    const fn = async () => {
+      throw new Error('some error');
+    };
+    ctx.runInBackground(fn());
+  });
+});
